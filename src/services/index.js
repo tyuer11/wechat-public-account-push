4d3b6b7b5ef0b1c7f442d5f011c2aa4fdc311363
tyuer11 <86798267+tyuer11@users.noreply.github.com>
2022-09-08 11:19:14 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -309,7 +309,7 @@ export const sendMessage = async (templateId, user, accessToken, params) => {
   const data = {
     "touser": user.id,
     "template_id": templateId,
-    "url": user.openUrl || "https://wangxinleo.cn",
+    "url": user.openUrl || "https://www.yuque.com/ran1364",
     "topcolor": "#FF0000",
     "data": wxTemplateData
   }
